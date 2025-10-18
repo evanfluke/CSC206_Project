6034e65a9c2ffcf86134562ee57f33390dec5187
evanfluke <eafluke@geneva.edu>
2025-10-18 01:38:20 -0400

    project commit 2

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/')
+def buy():
+    return render_template('buy.html')
+
+@app.route('/')
+def sell():
+    return render_template('sell.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
